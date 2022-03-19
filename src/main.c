7c2cf56dce7143fd73d064e64857fa8c63152de7
stardust <stardustfxprivate@gmail.com>
2022-03-19 10:03:55 -0700

    Declared global variables and created invidiual functions for updating and drawing the game.

--- src/main.c
+++ src/main.c
@@ -1,31 +1,52 @@
 #include "raylib.h"
 
+// Initialize
+static const int screenWidth = 1920;
+static const int screenHeight = 1080;
+static const int frameRate = 144; 
+
+static Rectangle playerOnePaddle = { 100.0f, (float)screenHeight / 2, 30, 200 };
+static Rectangle playerTwoPaddle = { screenWidth - 200, (float)screenHeight / 2, 30, 200 };
+
+static Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2};
+static Vector2 ballVelocity = { -5.0f, 5.0f }; // Initial velocity heads towards playerOne
+
+static void InitGame(void);
+static void UpdateGame(void);
+static void DrawGame(void);
+static void UpdateDrawFrame(void);
+
 int main(void)
 {
-    // Initialize
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
-    const int frameRate = 144; 
-
     InitAudioDevice();
     InitWindow(screenWidth, screenHeight, "Pong");
-
-    Rectangle playerOnePaddle = { 100.0f, (float)screenHeight / 2, 30, 200 };
-    Rectangle playerTwoPaddle = { screenWidth - 200, (float)screenHeight / 2, 30, 200 };
-
-    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2};
-    // Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight};
-    Vector2 ballVelocity = { -5.0f, 5.0f }; // Initial velocity heads towards playerOne
-
-    Sound paddleHitSound = LoadSound("C:\\Users\\basti\\Documents\\Coding Projects\\C++\\Pong\\Resources\\boop.wav");
+    InitGame();
 
     SetTargetFPS(frameRate);
 
     // Main game loop
     while (!WindowShouldClose())
     {
-        // TODO: collision check
-        if (IsKeyDown(KEY_UP))
+        UpdateDrawFrame();
+    }
+
+    // UnloadSound(paddleHitSound);
+
+    CloseAudioDevice();
+    CloseWindow();
+        
+    return 0;
+}
+
+void InitGame(void)
+{
+
+}
+
+void UpdateGame(void)
+{
+
+    if (IsKeyDown(KEY_UP))
         {
             playerOnePaddle.y = ( (playerOnePaddle.y - 4.0f) < 0.0f) ? playerOnePaddle.y : playerOnePaddle.y - 4.0f;
         }
@@ -39,7 +60,7 @@ int main(void)
         {
             ballVelocity.x = -(ballVelocity.x);
             ballVelocity.y = -(ballVelocity.y);
-            PlaySound(paddleHitSound);
+            // PlaySound(paddleHitSound);
         }
         // Ball collides with top of screen
         if ( (ballPosition.y == screenHeight) || (ballPosition.y == 0)) ballVelocity.y = -(ballVelocity.y);
@@ -59,20 +80,20 @@ int main(void)
             ballVelocity.x = -5.0f;
             ballVelocity.y = 5.0f;
         }
+}
 
-        // Draw graphics
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawRectangleRec(playerOnePaddle, WHITE);
-            DrawRectangleRec(playerTwoPaddle, WHITE);
-            DrawCircleV(ballPosition, 25.0f, WHITE);
-        EndDrawing();
-    }
+void DrawGame(void)
+{
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawRectangleRec(playerOnePaddle, WHITE);
+    DrawRectangleRec(playerTwoPaddle, WHITE);
+    DrawCircleV(ballPosition, 25.0f, WHITE);
+    EndDrawing();
+}
 
-    UnloadSound(paddleHitSound);
-
-    CloseAudioDevice();
-    CloseWindow();
-        
-    return 0;
+void UpdateDrawFrame(void)
+{
+    UpdateGame();
+    DrawGame();
 }
